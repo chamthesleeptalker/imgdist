bf02731f7241d1e30379b9999ef256e1c900f61d
Charmyne Mamador <charmynemamador@gmail.com>
2016-05-23 13:19:42 +0800

    remove lodash

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'static/dist')));
+app.use(express.static(path.join(__dirname, 'static/dist'))); 
 
 app.use('/', routes);
 app.use('/users', users);
