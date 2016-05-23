a989e2837c4e9561cfcebc5563d6aa330d07286e
Charmyne Mamador <charmynemamador@gmail.com>
2016-05-23 13:11:34 +0800

    removed lodash from app.js

--- app.js
+++ app.js
@@ -9,8 +9,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-var app = express(_);
-var _ = require('lodash');
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
